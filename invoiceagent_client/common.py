e53138ffb0ff3e49400aef468bc40388cb8bce3e
takao-s <takao.sekiguchi@gmail.com>
2023-05-22 16:52:45 +0900

    _downloadの例外処理追加

--- invoiceagent_client/common.py
+++ invoiceagent_client/common.py
@@ -38,6 +38,12 @@ class CommonMixin():
 
         r = self._request(method=method, path=path, headers=headers, data=data, stream=True)
 
+        if r.status_code != 200:
+            logger.error(f"HTTP Status Code: {r.status_code}")
+            for headerkey in [item for item in r.headers.keys() if item.startswith('X-Spa-Error')]:
+                logger.error(f"{headerkey}: {r.headers[headerkey]}")
+            raise
+
         content_type = r.headers['Content-Type']
         content_disp = r.headers.get('Content-Disposition')
         for chunk in r.iter_content(chunk_size=8192):
